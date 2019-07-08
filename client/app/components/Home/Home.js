c763e1d43e62deb70a68bbf6849400abdee7ae24
MichaelSam <michaelsam08@gmail.com>
2019-07-08 12:52:30 -0500

    whateva

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -1,24 +1,103 @@
-import React, { Component } from 'react';
-import 'whatwg-fetch';
+import React, { Component } from "react";
+import "whatwg-fetch";
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      counters: []
+      counters: [],
+      isLoading: true,
+      token: "",
+      signUpError: "",
+      signInError: "",
+      signInEmail: "",
+      signInPassword: "",
+      signUpEmail: "",
+      signUpPassword: ""
     };
-
+    this.onTextboxChangeSignInEmail = this.onTextboxChangeSignInEmail.bind(
+      this
+    );
+    this.onTextboxChangeSignInPassword = this.onTextboxChangeSignInPassword.bind(
+      this
+    );
+    this.onTextboxChangeSignUpEmail = this.onTextboxChangeSignUpEmail.bind(
+      this
+    );
+    this.onTextboxChangeSignUpPassword = this.onTextboxChangeSignUpPassword.bind(
+      this
+    );
     this.newCounter = this.newCounter.bind(this);
     this.incrementCounter = this.incrementCounter.bind(this);
     this.decrementCounter = this.decrementCounter.bind(this);
     this.deleteCounter = this.deleteCounter.bind(this);
 
     this._modifyCounter = this._modifyCounter.bind(this);
+    this.onSignUp = this.onSignUp.bind(this);
   }
-
   componentDidMount() {
-    fetch('/api/counters')
+    this.setState({
+      isLoading: false
+    });
+  }
+  onTextboxChangeSignInEmail(event) {
+    this.setState({
+      signInEmail: event.target.value
+    });
+  }
+  onTextboxChangeSignInPassword(event) {
+    this.setState({
+      signInPassword: event.target.value
+    });
+  }
+  onTextboxChangeSignUpEmail(event) {
+    this.setState({
+      signUpEmail: event.target.value
+    });
+  }
+  onTextboxChangeSignUpPassword(event) {
+    this.setState({
+      signUpPassword: event.target.value
+    });
+  }
+  onSignUp(event) {
+    // Grab state
+    const { signUpEmail, signUpPassword } = this.state;
+    this.setState({
+      isLoading: true
+    });
+    // Post request to backend
+    fetch("/api/account/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: signUpEmail,
+        password: signUpPassword
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log("json", json);
+        if (json.success) {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+            signUpEmail: "",
+            signUpPassword: ""
+          });
+        } else {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false
+          });
+        }
+      });
+  }
+  componentDidMount() {
+    fetch("/api/counters")
       .then(res => res.json())
       .then(json => {
         this.setState({
@@ -28,7 +107,7 @@ class Home extends Component {
   }
 
   newCounter() {
-    fetch('/api/counters', { method: 'POST' })
+    fetch("/api/counters", { method: "POST" })
       .then(res => res.json())
       .then(json => {
         let data = this.state.counters;
@@ -43,7 +122,7 @@ class Home extends Component {
   incrementCounter(index) {
     const id = this.state.counters[index]._id;
 
-    fetch(`/api/counters/${id}/increment`, { method: 'PUT' })
+    fetch(`/api/counters/${id}/increment`, { method: "PUT" })
       .then(res => res.json())
       .then(json => {
         this._modifyCounter(index, json);
@@ -53,7 +132,7 @@ class Home extends Component {
   decrementCounter(index) {
     const id = this.state.counters[index]._id;
 
-    fetch(`/api/counters/${id}/decrement`, { method: 'PUT' })
+    fetch(`/api/counters/${id}/decrement`, { method: "PUT" })
       .then(res => res.json())
       .then(json => {
         this._modifyCounter(index, json);
@@ -63,10 +142,9 @@ class Home extends Component {
   deleteCounter(index) {
     const id = this.state.counters[index]._id;
 
-    fetch(`/api/counters/${id}`, { method: 'DELETE' })
-      .then(_ => {
-        this._modifyCounter(index, null);
-      });
+    fetch(`/api/counters/${id}`, { method: "DELETE" }).then(_ => {
+      this._modifyCounter(index, null);
+    });
   }
 
   _modifyCounter(index, data) {
@@ -84,25 +162,93 @@ class Home extends Component {
   }
 
   render() {
+    const {
+      isLoading,
+      token,
+      signInError,
+      signInEmail,
+      signInPassword,
+      signUpEmail,
+      signUpPassword,
+      signUpError
+    } = this.state;
+    if (isLoading) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
+    }
+    if (!token) {
+      return (
+        <div>
+          <div>
+            {signInError ? <p>{signInError}</p> : null}
+            <p>Sign In</p>
+            <input
+              type="email"
+              placeholder="Email"
+              value={signInEmail}
+              onChange={this.onTextboxChangeSignInEmail}
+            />
+            <br />
+            <input
+              type="password"
+              placeholder="Password"
+              value={signInPassword}
+              onChange={this.onTextboxChangeSignInPassword}
+            />
+            <br />
+            <button>Sign In</button>
+          </div>
+          <br />
+          <br />
+          <div>
+            {signUpError ? <p>{signUpError}</p> : null}
+            <p>Sign Up</p>
+            <input
+              type="email"
+              placeholder="Email"
+              value={signUpEmail}
+              onChange={this.onTextboxChangeSignUpEmail}
+            />
+            <br />
+            <input
+              type="password"
+              placeholder="Password"
+              value={signUpPassword}
+              onChange={this.onTextboxChangeSignUpPassword}
+            />
+            <br />
+            <button onClick={this.onSignUp}>Sign Up</button>
+          </div>
+        </div>
+      );
+    }
     return (
-      <>
-        <p>Counters:</p>
-
-        <ul>
-          { this.state.counters.map((counter, i) => (
-            <li key={i}>
-              <span>{counter.count} </span>
-              <button onClick={() => this.incrementCounter(i)}>+</button>
-              <button onClick={() => this.decrementCounter(i)}>-</button>
-              <button onClick={() => this.deleteCounter(i)}>x</button>
-            </li>
-          )) }
-        </ul>
-
-        <button onClick={this.newCounter}>New counter</button>
-      </>
+      <div>
+        <p>Signed in</p>
+      </div>
     );
   }
+  // return (
+  //   <>
+  //     <p>Counters:</p>
+
+  //     <ul>
+  //       {this.state.counters.map((counter, i) => (
+  //         <li key={i}>
+  //           <span>{counter.count} </span>
+  //           <button onClick={() => this.incrementCounter(i)}>+</button>
+  //           <button onClick={() => this.decrementCounter(i)}>-</button>
+  //           <button onClick={() => this.deleteCounter(i)}>x</button>
+  //         </li>
+  //       ))}
+  //     </ul>
+
+  //     <button onClick={this.newCounter}>New counter</button>
+  //   </>
+  // );
 }
 
 export default Home;
